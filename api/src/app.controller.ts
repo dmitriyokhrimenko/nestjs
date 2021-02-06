6a1f4cec66052b52de79606860623c2aee0449ad
dm.okhrimenko <dm.okhrimenko@gmail.com>
2021-02-06 02:02:59 +0200

    ref(Architecture): separate frontend and api

---
+++ api/src/app.controller.ts
@@ -0,0 +1,20 @@
+import { Get, Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @UseGuards(AuthGuard('local'))
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
+  }
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
